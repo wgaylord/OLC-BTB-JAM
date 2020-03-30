0fb0cf3f045b7fe8dd623323d4cf381431c42593
William Gaylord <chibill110@gmail.com>
2020-03-29 23:30:00 -0500

    Added somemore logic to zombies. Also implemented game logic only every 50ms to make input better.

--- entities.py
+++ entities.py
@@ -1,13 +1,94 @@
+from pygame import Surface,draw
+import random
+from tiles import tiles
+
 class Entity:
-	def __init__(self):
-		pass
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.health = 10.0
+        self.move_delay = 2
+        self.attack_delay = 1
+        self.attack_range = 1
+        self.attack_damage = 1
+        self.texture = Surface((16,16))
 
+    def draw(self):
+        return self.texture
+        
+    def attack(self,playerx,playery):
+        return 0
 
+    def hurt(self,inventory,current):
+        return
 
 
 class Zombie(Entity):
-    def __init__(self):
+    def __init__(self,x,y):
+        super().__init__()
+        self.x = x
+        self.y = y
         self.health = 10.0
-        self.x = 0
-        self.y = 0
-        self.attack_delay = 30
+        self.move_delay = 1
+        self.move_time = 0
+        self.attack_delay = 10
+        self.attack_range = 1
+        self.attack_damage = 1
+        self.attack_time = 0
+        self.texture.fill((128,0,0))
+        draw.rect(self.texture,(0,255,0),(6,6,3,3))
+        
+    def move(self,playerx,playery,world,entitys):
+        
+        if(self.move_time < self.move_delay):
+            self.move_time+=1
+            return
+        
+        def filled(entitys,x,y):
+            for z in entitys:
+                if z.x == x and z.y == y and z != self:
+                    return True
+            return False
+            
+        
+        if self.x < playerx:
+            self.x+= random.randint(0,1)
+            if tiles[world[self.x][self.y]].solid or (self.x == playerx and  self.y == playery) or filled(entitys,self.x,self.y):
+                self.x-=1
+            else:
+                return
+                    
+        elif self.x > playerx:
+            self.x-= random.randint(0,1)
+            if tiles[world[self.x][self.y]].solid  or (self.x == playerx and  self.y == playery) or filled(entitys,self.x,self.y):
+                self.x+=1
+            else:
+                return
+        if self.y < playery:
+            self.y+= random.randint(0,1)
+            if tiles[world[self.x][self.y]].solid or (self.x == playerx and  self.y == playery) or filled(entitys,self.x,self.y):
+                self.y-=1
+            else:
+                return
+        elif self.y > playery:
+            self.y-= random.randint(0,1)
+            if tiles[world[self.x][self.y]].solid or (self.x == playerx and  self.y == playery) or filled(entitys,self.x,self.y):
+                self.y+=1
+            else:
+                return
+        
+        
+        
+    def attack(self,playerx,playery):
+        
+        if abs(playerx-self.x) <= self.attack_range and abs(playery - self.y) <= self.attack_range:
+            if self.attack_time >= self.attack_delay:
+                self.attack_time = 0
+                return 1
+            else:
+                self.attack_time +=1
+        return 0
+        
+    def hurt(self,inventory,current):
+        self.health-=5
+        return
--- game.py
+++ game.py
@@ -3,6 +3,7 @@ import time
 import random
 from pygame import Rect , draw
 from tiles import tiles,player
+import entities
 import json
 
 full = True
@@ -11,22 +12,23 @@ pygame.init()
 pygame.font.init()
 pygame.mouse.set_cursor(*pygame.cursors.diamond)
 
-def text_to_screen(screen, text, x, y, size = 50,color = (200, 000, 000)):
+def text_to_screen(screen1, text, x, y, size = 50,color = (200, 000, 000)):
     try:
 
         text = str(text)
         font = pygame.font.SysFont('Comic Sans MS', size)
         text = font.render(text, True, color)
-        screen.blit(text, (x, y))
+        screen1.blit(text, (x, y))
 
     except Exception as e:
-        pass
+        print(e)
 
 size = width, height = 640, 480
 
 screen_center = (width//2,height//2)
 
-screen = pygame.display.set_mode(size,pygame.FULLSCREEN)
+
+screen = pygame.display.set_mode(size,pygame.FULLSCREEN|pygame.HWSURFACE )
 
 screen_tile_width = width//16
 screen_tile_height = height//16
@@ -43,7 +45,9 @@ worldSurface.fill((0,255,0))
 
 PlayerX = world_width//2
 PlayerY = world_height//2
+PlayerHealth = 10
 
+entitys = []
 
 
 world = []
@@ -103,9 +107,13 @@ currentInventorySelected = 0
 
 t = time.time()
 
+overlay = pygame.Surface((width, height-32))
 
-
-
+GameOver = pygame.Surface((width, height))
+text_to_screen(GameOver,"GAME",200,200,50,(255,0,0))
+text_to_screen(GameOver,"OVER",200,250,50,(255,0,0))
+text_to_screen(GameOver,"Press x to exit",200,300,30,(255,255,255))
+pygame.time.set_timer(25, 50)
 count = 0
 key = 0
 while 1:
@@ -139,46 +147,75 @@ while 1:
                 x = ((event.pos[0] - (width//2))//16)
                 y = ((event.pos[1] - (height//2))//16)
                 if abs(x) < 5 and abs(y) < 5:
-                    world,inventory,currentInventorySelected = tiles[world[PlayerX+x][PlayerY+y]].onClick(event.button,world,PlayerX+x,PlayerY+y,inventory,currentInventorySelected)
-    if key == 119:
-        PlayerY-=1
-        if tiles[world[PlayerX][PlayerY]].solid:
-            PlayerY+=1
-    if key == 115:
-        PlayerY+=1
-        if tiles[world[PlayerX][PlayerY]].solid:
-            PlayerY-=1
+                    hit = False
+                    for z in entitys:
+                        #print(z.x,z.y,x,y)
+                        if z.x == PlayerX+x and z.y == PlayerY+y:
+                            z.hurt(inventory,currentInventorySelected)
+                            hit = True
+                    if not hit:
+                        world,inventory,currentInventorySelected = tiles[world[PlayerX+x][PlayerY+y]].onClick(event.button,world,PlayerX+x,PlayerY+y,inventory,currentInventorySelected)
+        if event.type == 25:
+            if key == 119:
+                PlayerY-=1
+                if tiles[world[PlayerX][PlayerY]].solid:
+                    PlayerY+=1
+            if key == 115:
+                PlayerY+=1
+                if tiles[world[PlayerX][PlayerY]].solid:
+                    PlayerY-=1
+            
+            if key == 97:
+                PlayerX-=1
+                if tiles[world[PlayerX][PlayerY]].solid:
+                    PlayerX+=1
+            if key == 100:
+                PlayerX+=1
+                if tiles[world[PlayerX][PlayerY]].solid:
+                    PlayerX-=1
+            if key == 113:
+                if currentInventorySelected > 0:
+                    currentInventorySelected-=1
+                key = 0
+            if key == 101:
+                if currentInventorySelected < len(inventory)-1:
+                    currentInventorySelected+=1
+                key = 0
+            if currentInventorySelected > len(inventory)-1: 
+                currentInventorySelected = len(inventory)-1
+                if currentInventorySelected < 0:
+                    currentInventorySelected =0
+            if key == 292:
+                if full == False:
+                    full = True
+                    screen = pygame.display.set_mode(size,pygame.FULLSCREEN|pygame.HWSURFACE)
+                else:
+                    screen = pygame.display.set_mode(size)
+                    full = False
+                key = 0
+            if key == 120 and PlayerHealth <= 0:
+                pygame.quit()
+                exit()
+
+            time_of_day = abs(int(((449000+pygame.time.get_ticks()) % 900000) / 2250)-200)
+            
+            index = 0
+            for z in entitys:
+                if z.health <=0:
+                    del entitys[index]
+                    continue
+                index+=1
+                z.move(PlayerX,PlayerY,world,entitys)
+                PlayerHealth-=z.attack(PlayerX,PlayerY)
+            
+            if len(entitys) < 30 and time_of_day > 100:
+                x,y = random.randint(16,279),random.randint(16,220)
+                while tiles[world[x][y]].solid:
+                    x,y = random.randint(16,279),random.randint(16,220)
+                entitys.append(entities.Zombie(x,y))
+        
     
-    if key == 97:
-        PlayerX-=1
-        if tiles[world[PlayerX][PlayerY]].solid:
-            PlayerX+=1
-    if key == 100:
-        PlayerX+=1
-        if tiles[world[PlayerX][PlayerY]].solid:
-            PlayerX-=1
-    if key == 113:
-        if currentInventorySelected > 0:
-            currentInventorySelected-=1
-        key = 0
-    if key == 101:
-        if currentInventorySelected < len(inventory)-1:
-            currentInventorySelected+=1
-        key = 0
-    if currentInventorySelected > len(inventory)-1: 
-        currentInventorySelected = len(inventory)-1
-        if currentInventorySelected < 0:
-            currentInventorySelected =0
-    if key == 292:
-        if full == False:
-            full = True
-            screen = pygame.display.set_mode(size,pygame.FULLSCREEN)
-        else:
-            screen = pygame.display.set_mode(size)
-        key = 0
-   # print(key)
-
-
+            draw.rect(overlay,(time_of_day,time_of_day,time_of_day),(0,0,width,height-32))    
 
     
     Top = PlayerY-15
@@ -187,6 +224,11 @@ while 1:
     for x in range(40):
         for y in range(30):
             screen.blit(tiles[world[Left+x][Top+y]].draw(world,PlayerX,PlayerY,Left+x,Top+y),(x*16,y*16))
+            for z in entitys:
+                if (Left+x) == z.x and (Top+y) == z.y:
+                    screen.blit(z.draw(),(x*16,y*16))
+            
+    
     
     screen.blit(player,screen_center)
     
@@ -195,6 +237,10 @@ while 1:
     
     draw.rect(screen,(128,0,128),(((currentInventorySelected+1)*32)-8,height-30,30,30))
     
+    
+    
+    screen.blit(overlay,(0,0),special_flags=pygame.BLEND_RGB_SUB)
+    text_to_screen(screen,"Health: "+str(PlayerHealth),10,10,20,(255,0,0))
     
     offset = 32
 
@@ -207,9 +253,15 @@ while 1:
     
 
     count+=1
+    
+    
     if(time.time()-t > 1):
         pygame.display.set_caption(str(count) + " FPS")
         count = 0
         t = time.time()
-    pygame.time.wait(35)
+    if PlayerHealth <=0:
+        screen.blit(GameOver,(0,0))
+        
+        
     pygame.display.flip()
+    
